636e4b1a6506a223208ce65ee16136acf396aa95
vadim-mg <vadikm1981@yandex.ru>
2026-09-18 22:06:43 +0500

    install: settings for building

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,30 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
 
 export default defineConfig({
-  root: '.',
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    cssMinify: false
-  },
-  css: {
-    devSourcemap: true,
-    preprocessorOptions: {
-      scss: {
-        sourceMap: true
-      }
+    root: '.',
+    base: './',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: true,
+        cssMinify: false
+    },
+    css: {
+        devSourcemap: true,
+        preprocessorOptions: {
+            scss: {
+                sourceMap: true,
+            }
+        }
+    },
+    server: {
+        port: 5173,
+        open: true,
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
 });
