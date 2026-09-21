f38ff18492e19c368406f86fa4a2e30f6d42d27e
vadim-mg <vadikm1981@yandex.ru>
2026-09-21 21:41:28 +0500

    fix: paths

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
     root: '.',
-    base: './',
+    base: '/rsschool-landing-page/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
