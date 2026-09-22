5ef4d1c2ddc4d032f657bb29499af54d3859ea82
vadim-mg <vadikm1981@yandex.ru>
2026-09-22 11:31:16 +0500

    feat: smooth scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
 import '../scss/main.scss';
+
+/* Обработчик для плавной прокрутки */
+document.querySelectorAll('a[href*="#"]').forEach(a => {
+    a.onclick = e => {
+        const t = document.getElementById(a.hash.slice(1));
+        if (!t) return;
+        e.preventDefault();
+
+        const start = scrollY, end = t.offsetTop, duration = 1000;
+        const t0 = performance.now();
+
+        (function step(now) {
+            const p = Math.min((now - t0) / duration, 1);
+            scrollTo(0, start + (end - start) * p * (2 - p));
+            if (p < 1) requestAnimationFrame(step);
+        })(t0);
+    };
+});
