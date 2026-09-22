455af84407a72cd2a71184a3e855651529e816b2
vadim-mg <vadikm1981@yandex.ru>
2026-09-22 13:17:28 +0500

    fix: vite.config

--- vite.config.js
+++ vite.config.js
@@ -31,8 +31,8 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                menu: resolve(__dirname, 'menu.html'),
+                main: resolve(import.meta.dirname, 'index.html'),
+                menu: resolve(import.meta.dirname, 'menu.html'),
             }
         }
     }
