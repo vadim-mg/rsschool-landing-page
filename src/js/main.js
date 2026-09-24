554a6c1ed0c5afb05886c3716ea9a0a3ae57574a
vadim-mg <vadikm1981@yandex.ru>
2026-09-24 08:48:04 +0500

    fix: semicolon bug

--- src/js/main.js
+++ src/js/main.js
@@ -42,13 +42,13 @@ document.querySelectorAll('a[href*="#"]').forEach(a => {
         e.preventDefault()
 
         const start = scrollY, end = t.offsetTop, duration = 1000
-        const t0 = performance.now()
+        const t0 = performance.now();
 
-            (function step(now) {
-                const p = Math.min((now - t0) / duration, 1)
-                scrollTo(0, start + (end - start) * p * (2 - p))
-                if (p < 1) requestAnimationFrame(step)
-            })(t0)
+        (function step(now) {
+            const p = Math.min((now - t0) / duration, 1)
+            scrollTo(0, start + (end - start) * p * (2 - p))
+            if (p < 1) requestAnimationFrame(step)
+        })(t0)
     }
 })
 
